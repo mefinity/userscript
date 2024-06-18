1e2ca20ff193f09c92ec4bc6b8e10e849989c73e
MeFinity <72972952+mefinity@users.noreply.github.com>
2024-06-18 23:25:22 +0700

    Create elaelo-able.user.js

---
+++ elaelo-able.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Cure Elaelo Disability
+// @namespace   Violentmonkey script
+// @version     1.0
+// @description Delete 'disabled="disabled"' on elaelo
+// @author      MeFinity
+// @homepageURL https://github.com/MeFinity/userscript
+// @downloadURL  https://github.com/MeFinity/userscript/raw/main/elaelo-able.user.js
+// @updateURL    https://github.com/MeFinity/userscript/raw/main/elaelo-able.user.js
+// @match       https://elaelo.com/*
+// @match       https://elaelo.id/*
+// @grant       none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function curedisability() {
+    const buttons = document.querySelectorAll('button[disabled="disabled"]');
+    for (const button of buttons) {
+      button.removeAttribute('disabled');
+    }
+  }
+
+  curedisability();
+  setInterval(curedisability, 3000);
+})();
